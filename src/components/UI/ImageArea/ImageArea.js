57ffbf3f8e051866159495d8fe773b902785e09d
raw-at <abhinavrawat728@gmail.com>
2018-02-08 20:14:03 +0530

    footer added

--- src/components/UI/ImageArea/ImageArea.js
+++ src/components/UI/ImageArea/ImageArea.js
@@ -3,7 +3,9 @@ import classes from './ImageArea.css';
 import card_1  from '../../../assets/card_1.png';
 import card_2  from '../../../assets/card_2.png';
 import card_3  from '../../../assets/card_3.png';
+import logo from '../../../assets/logo.png';
 import Background from '../../../assets/landing.png';
+import AppImage from '../../../assets/ios-android.png';
 const imageArea = (props) => {
   const setStyle = {
     backgroundImage: "url(" + Background + ")",
@@ -18,10 +20,22 @@ const imageArea = (props) => {
     <div className={classes.left_box}>
       <h2>Heyo ! <span style={{color:'orange'}}>Friends</span></h2>
       <p>
-        We are here for you to enhance your prepration process for SSC exams.
-        What we can provide is quite diverse and yeah its Totally FREE !
-        We provide you various things raning from test series,sample questions
-        chapter wise as well in depth analysis.
+                  We are here for you to enhance your prepration process for SSC exams.
+                  What we can provide is quite diverse and yeah its Totally FREE !
+                  We provide you various things raning from test series,sample questions
+                  chapter wise as well in depth analysis.
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+                 when an unknown prindustrys standard dummy text ever since the 1500s,
+                 when an unknown printer took a galley of type and scrambled it to make a
+                 type specimen bookinter took a galley of type and scrambled it to make a
+                 type specimen book.
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+                 when an unknown prindustrys standard dummy text ever since the 1500s,
+                 when an unknown printer took a galley of type and scrambled it to make a
+                 type specimen bookinter took a galley of type and scrambled it to make a
+                 type specimen book.
         <i className="far fa-heart fa-2x"></i>
       </p>
     </div>
@@ -71,6 +85,67 @@ const imageArea = (props) => {
             </p>
         </div>
     </div>
+
+    <div className={classes.footer}>
+      <div className={classes.footer_left}>
+            <img src={logo} />
+            <br />
+            <br />
+            <br />
+            
+            <table>
+              <tbody>
+                <tr>
+                  <td><i className="fa fa-2x 	fa fa-id-card-o"></i></td>
+                  <td style={{"paddingLeft":"5%"}}>#803-805, The Landmark Building,
+                      (Above Croma), Sector 7,
+                      Kharghar, Navi Mumbai - 410210
+              </td>
+                </tr>
+                <tr>
+                  <td><i className="fa fa-2x fa-envelope-o"></i></td>
+                  <td style={{"paddingLeft":"5%"}}>support@testbook.com</td>
+                </tr>
+                <tr>
+                  <td><i className="fa fa-3x fa-mobile-phone"></i></td>
+                  <td style={{"paddingLeft":"5%"}}> +91-8080-433-233</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td style={{"paddingLeft":"5%"}}>
+                    Office hours: 10 AM to 8 PM (Sunday Closed)
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            
+          
+      </div>
+      <div className={classes.footer_right}>
+            <div>
+              <img src={AppImage}/>
+
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td><i className="fa fa-facebook-official" style={{"fontSize":"48px"}}></i></td>
+                    <td><i className="fa fa-instagram" style={{"fontSize":"48px"}}></i></td>
+                    <td><i className="fa fa-linkedin" style={{"fontSize":"48px"}}></i></td>
+                    <td><i className="fa fa-google-plus" style={{"fontSize":"48px"}}></i></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            
+            
+
+      </div>
+    </div>
+
+  
+
     </div>)
 };
 
