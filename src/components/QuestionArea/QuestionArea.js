cba58b3f459ee0b38723b969592aaf7392d1093b
raw-at <abhinavrawat2308@gmail.com>
2018-03-02 10:34:48 +0530

    radioButton and answer checking is initiated

--- src/components/QuestionArea/QuestionArea.js
+++ src/components/QuestionArea/QuestionArea.js
@@ -7,9 +7,13 @@ import {Link} from 'react-router-dom';
 import Spinner from '../UI/Spinner/Spinner';
 import { withRouter } from 'react-router-dom';
 import * as actions from '../../store/actions/index';
+import Aux from '../../hoc/Aux/Aux';
 class QuestionArea extends Component {
     state = {
-        closeClicked:false
+        closeClicked:false,
+        checkBoxValue:null,
+        userInAnswerState:true,
+        //questionVisibility:true,
     }
     current_question_number = parseInt(this.props.match.params.question) - 1
     
@@ -31,10 +35,32 @@ class QuestionArea extends Component {
     previousQuestion = () => {
         this.current_question_number -= 1
         const final_url = this.url_with_question_number + (this.current_question_number).toString()
-        const next_url = this.props.location.pathname.slice(0,this.props.location.pathname.length-1)+(this.current_question_number+1).toString()
+        const prev_url = this.props.location.pathname.slice(0,this.props.location.pathname.length-1)+(this.current_question_number+1).toString()
        // console.log(this.current_question_number)
         this.props.nextQuestion(final_url)
+        window.location = prev_url
         //this.props.history.push(next_url)
+    }
+
+    checkAnswer = () => {
+        if(this.props.question.answer == this.state.checkBoxValue ){
+            console.log('correct')
+            this.setState({
+                userInAnswerState:false,
+                //questionVisibility:!this.state.questionVisibility
+            })
+        }
+        else {
+            console.log('incorrect')
+            this.setState({userInAnswerState:false})
+        }
+        
+    }
+
+    setRadio = (event)=> {
+        this.setState({
+            checkBoxValue:event.currentTarget.value
+        })
     }
     /*componentWillMount() {
         this.unlisten = this.props.history.listen((location, action) => {
@@ -44,26 +70,53 @@ class QuestionArea extends Component {
       */
       
     render(){
-       console.log(this.current_question_number)
+       
         let question = null;
         let options = <Spinner />;
+        let question_number = null;
+        let buttons = null;
         if(this.props.question){
-        question = this.props.question.question
-        options = this.props.question.options.map(option=>{
-            return (
-                <label className={classes.container} key={this.props.question.options.indexOf(option)}>
-                <span className={classes.option}>{option}</span>
-                    <input type="radio" name="radio"/>
-                    <span className={classes.checkmark}></span>
-                </label>
-               
-            )
-        })        }
+            if(this.state.userInAnswerState){
+                question = this.props.question.question
+            
+                options = this.props.question.options.map(option=>{
+                    return (
+                        <label className={classes.container} key={this.props.question.options.indexOf(option)}>
+                        <span className={classes.option}>{option}</span>
+                            <input type="radio" name="radio" value={option} onChange={this.setRadio}/>
+                            <span className={classes.checkmark}></span>
+                        </label>
+                    
+                    )
+                })
+                question_number = (
+                    <span style={{"fontSize":"28px"}}>
+                                Question {this.current_question_number+1}:
+                            </span>
+                            
+                )
+
+                buttons = (
+                            <Aux>
+                            <Button btnType="Danger" clicked={this.previousQuestion} disabled={this.current_question_number<=0?true:false}> &nbsp;Previous</Button>
+                            <Button btnType="Success" clicked={this.checkAnswer}> &nbsp;Submit</Button>
+                            <Button btnType="Warning" clicked={this.nextQuestion} >Skip</Button>
+                            </Aux>
+                )
+            }
+            else{
+                options = (
+                    <Aux>
+                        <h1>Yos</h1>
+                    </Aux>
+                )
+            }
+        }
         return (
             <div className={classes.QuestionArea}>
                 <div className={classes.Question}>
                     <div>
-                        <span style={{"fontSize":"28px"}}>Question {this.current_question_number+1}:</span>
+                        {question_number}
                         <span style={{"float":"right"}} className={classes.close} >
                             <i className="fa fa-close" onClick={this.closeClickedHandler}></i>
                         </span>
@@ -82,10 +135,8 @@ class QuestionArea extends Component {
                         
                     </form>
                     <div style={{"marginLeft":"20%"}}>
-                        <Button btnType="Danger" clicked={this.previousQuestion} disabled={this.current_question_number<=0?true:false}> &nbsp;Previous</Button>
-                        <Button btnType="Success"> &nbsp;Submit</Button>
-                        <Button btnType="Warning" clicked={this.nextQuestion} >Skip</Button>
-                        </div> 
+                        {buttons}    
+                    </div> 
                 </div>
             </div>
         )
